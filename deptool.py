500684c45000f9454df2de64516806c6c37f95e4
Yevgen ZK <zilinchuk@gmail.com>
2016-04-02 15:26:28 -0700

    initial commit

---
+++ deptool.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+def take_info():
+	config_path = raw_input("Please enter path to config file: ")
+	build_version = raw_input("Please enter build version: ")
+	source_path = raw_input("PLease enter full path to traballs: ")
+
